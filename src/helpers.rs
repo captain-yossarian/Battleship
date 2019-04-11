521c2316230b008acaebbc8b62c7b4350ff6375b
Serhjii Bilyk <sergiybiluk@gmail.com>
2019-04-11 21:49:46 +0200

    Add [rand] crate

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,15 +1,19 @@
-use std::collections::HashMap;
 
+use std::collections::HashMap;
+extern crate rand;
+use rand::{Rng,thread_rng};
 #[derive(Debug)]
-pub struct Ship {
-  size: i32,
-  direction: ShipDirection,
+pub enum ShipDirection {
+  Up,
+  Down,
+  Left,
+  Right,
 }
 
 #[derive(Debug)]
-pub enum ShipDirection {
-  Vertical,
-  Horizontal,
+pub struct Ship {
+  size: usize,
+  direction: ShipDirection,
 }
 
 const LEN: usize = 10;
@@ -17,7 +21,7 @@ const LEN: usize = 10;
 type Field = [[i32; LEN]; LEN];
 pub struct GameField {
   field: Field,
-  ships: HashMap<i32, i32>,
+  ships: HashMap<usize, usize>,
 }
 
 impl GameField {
@@ -44,12 +48,12 @@ impl GameField {
     }
   }
 
-  fn reduce_ships(&mut self, size: &i32) {
+  fn reduce_ships(&mut self, size: &usize) {
     let val = self.ships.get_mut(&size).unwrap();
     *val -= 1;
     println!("Val {}", val);
   }
-  pub fn create_ship(&mut self, size: i32, direction: ShipDirection) -> Option<Ship> {
+  pub fn create_ship(&mut self, size: usize, direction: ShipDirection) -> Option<Ship> {
     let allow = self.ships.get(&size).unwrap() > &0;
     if allow == true {
       self.reduce_ships(&size);
@@ -57,5 +61,48 @@ impl GameField {
     } else {
       None
     }
+  }
+
+  pub fn set_ship(&mut self, ship: Ship) {
+    println!("SHip direction {:?}", ship.direction);
+    println!("Enum {:?}", ShipDirection::Up);
+
+   // let mut rng = rand::random::<usize>();
+    //let roll = rng.gen_range(1, 7);
+
+    let quadrant2 = LEN - ship.size;
+    let quadrant4 = ship.size - 1;
+    match (ship.direction) {   
+
+      (ShipDirection::Up) => {
+        let min_row_index = quadrant4;    
+        println!("Random {} {}",min_row_index,thread_rng().gen_range(min_row_index, LEN));
+    
+      }
+      (ShipDirection::Right) => {
+        let max_column_index = quadrant2;
+        println!("Random {}",thread_rng().gen_range(0, max_column_index));
+
+      }
+      (ShipDirection::Down) => {
+        let max_row_index = quadrant2;
+        println!("Random {}",thread_rng().gen_range(0, max_row_index));
+
+      }
+      (ShipDirection::Left) => {
+        let min_column_index = quadrant4;
+        println!("Random {}",thread_rng().gen_range(min_column_index, LEN));
+
+      }
+      _ => println!("Error"),
+    }
+
+    /*
+    if ship.direction == ShipDirection::Up {
+      let min_row_index = ship.size - 1;
+          println!("MIn row {:?}", min_row_index);
+
+    }
+    */
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
   let mut field = GameField::new();
   field.show();
 
-  let ship = field.create_ship(4, ShipDirection::Horizontal);
-  println!("Rev {:?}", ship);
+  let ship = field.create_ship(4, ShipDirection::Up);
+  field.set_ship(ship.unwrap());
+  //println!("Rev {:?}", ship);
 }
