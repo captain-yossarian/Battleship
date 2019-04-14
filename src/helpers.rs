576158ea9e09e93f0b87df4bbaecf62c17af69ce
Serhjii Bilyk <sergiybiluk@gmail.com>
2019-04-14 11:16:19 +0200

    Refactyor

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
-use std::ops::Range;
-extern crate rand;
+
 use rand::{thread_rng, Rng};
+use std::ops::{Range, Sub};
+
 #[derive(Debug)]
 pub enum ShipDirection {
   Horizontal,
@@ -14,6 +15,18 @@ enum ShipShape {
   Vertical,
 }
 
+enum Direction {
+  Up,
+  Right,
+  Down,
+  Left,
+}
+
+struct DrawStep {
+  direction: Direction,
+  cells: u8,
+  increment: i8,
+}
 
 #[derive(Debug)]
 pub struct Ship {
@@ -23,14 +36,37 @@ pub struct Ship {
 
 }
 
-
 #[derive(Debug)]
+pub enum Status {
+  Empty,
+  Ship,
+  Bound,
+  Kill,
+}
+
+#[derive(Debug, Clone)]
 pub struct Point {
   row: u8,
   column: u8,
 }
 
-const LEN: u8 = 10;
+impl Point {
+  pub fn up(&mut self) {
+    self.row -= 1;
+  }
+
+  pub fn right(&mut self) {
+    self.column += 1;
+  }
+  pub fn down(&mut self) {
+    self.row += 1;
+  }
+  pub fn left(&mut self) {
+    self.column -= 1;
+  }
+}
+
+const LEN: u8 = 12;
 
 type Field = [[u8; LEN as usize]; LEN as usize];
 pub struct GameField {
@@ -49,16 +85,22 @@ impl GameField {
     }
 
     GameField {
-      field: [[0; 10]; 10],
+      field: [[0; 12]; 12],
       ships,
     }
   }
   pub fn show(&self) {
-    let columns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    println!("col   {:?}", columns);
-    println!("------------------------------------");
+
+    let columns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    println!("col         {:?}", columns);
+    println!("            -------------------------------");
     for (index, elem) in self.field.iter().enumerate() {
-      println!("row:{} {:?}", index, elem);
+      if index <= 9 {
+        println!("row:{}    {:?}", index, elem);
+      }
+      if index > 9 {
+        println!("row:{}   {:?}", index, elem);
+      }
     }
   }
 
@@ -74,9 +116,8 @@ impl GameField {
     if allow == true {
       self.reduce_ships(&size);
       let mut random = thread_rng();
-      let quadrant2 = LEN - size;
-      let dynamic = random.gen_range(0, quadrant2);
-      let stat = random.gen_range(0, LEN);
+      let dynamic = random.gen_range(1, 12 - size);
+      let stat = random.gen_range(1, 11);
       let range = dynamic..dynamic + size;
       let point;
 
@@ -87,9 +128,10 @@ impl GameField {
             column: dynamic,
           };
           for index in range {
-            self.draw(stat, index);
+            self.draw(stat, index, Status::Ship);
           }
-          println!("Horizontal point {:?}", point);
+          let path = self.wrap_ship_horizontal(&size);
+          self.draw_cell(point.clone(), path);
         }
         ShipDirection::Vertical => {
           point = Point {
@@ -97,8 +139,10 @@ impl GameField {
             column: stat,
           };
           for index in range {
-            self.draw(index, stat);
+            self.draw(index, stat, Status::Ship);
           }
+          let path = self.wrap_ship_vertical(&size);
+          self.draw_cell(point.clone(), path);
         }
       }
       Some(Ship {
@@ -111,20 +155,122 @@ impl GameField {
     }
   }
 
-
-  pub fn draw(&mut self, x: u8, y: u8) {
-    if x != 0 {
-      self.field[(x - 1) as usize][y as usize] = 2;
-   
-    }
-   // self.field[x as usize][(y - 1) as usize] = 2;
-    self.field[x as usize][y as usize] = 1;
-  //  self.field[x as usize][(y + 1) as usize] = 2;
-
-    if x != 9 {
-      self.field[(x + 1) as usize][y as usize] = 2;
-    }
+  fn wrap_ship_horizontal(&self, size: &u8) -> Vec<DrawStep> {
+    let long_shot = size + 1;
+    vec![
+      DrawStep {
+        direction: Direction::Left,
+        cells: 1,
+        increment: -1,
+      },
+      DrawStep {
+        direction: Direction::Up,
+        cells: 1,
+        increment: -1,
+      },
+      DrawStep {
+        direction: Direction::Right,
+        cells: long_shot,
+        increment: 1,
+      },
+      DrawStep {
+        direction: Direction::Down,
+        cells: 2,
+        increment: 1,
+      },
+      DrawStep {
+        direction: Direction::Left,
+        cells: long_shot,
+        increment: -1,
+      },
+    ]
   }
 
+  fn wrap_ship_vertical(&self, size: &u8) -> Vec<DrawStep> {
+    let long_shot = size + 1;
+
+
+    vec![
+      DrawStep {
+        direction: Direction::Up,
+        cells: 1,
+        increment: -1,
+      },
+      DrawStep {
+        direction: Direction::Right,
+        cells: 1,
+        increment: 1,
+      },
+      DrawStep {
+        direction: Direction::Down,
+        cells: long_shot,
+        increment: 1,
+      },
+      DrawStep {
+        direction: Direction::Left,
+        cells: 2,
+        increment: -1,
+      },
+      DrawStep {
+        direction: Direction::Up,
+        cells: long_shot,
+        increment: -1,
+      },
+    ]
+  }
+  fn drawer(&mut self, mut callback: Box<FnMut() -> u8>) {
+    let result = callback();
+    println!("After {}", result);
+  }
+  fn draw_cell(&mut self, point: Point, path: Vec<DrawStep>) {
+    let Point {
+      mut row,
+      mut column,
+    } = point;
+
+    for step in path {
+      let DrawStep {
+        direction,
+        cells,
+        increment,
+      } = step;
+
+      match direction {
+        Direction::Up => {
+          for _ in 0..cells {
+            row -= 1;
+            self.draw(row, column, Status::Bound);
+          }
+        }
+        Direction::Left => {
+          for _ in 0..cells {
+            column -= 1;
+            self.draw(row, column, Status::Bound);
+          }
+        }
+        Direction::Right => {
+          for _ in 0..cells {
+            column += 1;
+            self.draw(row, column, Status::Bound);
+          }
+        }
+        Direction::Down => {
+          for _ in 0..cells {
+            row += 1;
+            self.draw(row, column, Status::Bound);
+          }
+        }
+
+      }
+    }
+
+  }
+
+  pub fn draw(&mut self,point:Point, status: Status) {
+    let Point {
+      row, column
+    } = point;
+    self.field[row as usize][column as usize] = status as u8;
+  }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use helpers::{GameField, ShipDirection};
 fn main() {
   let mut field = GameField::new();
 
-  let ship = field.create_ship(3, ShipDirection::Horizontal);
+  let ship = field.create_ship(2, ShipDirection::Horizontal);
   field.show();
 
   //println!("Rev {:?}", ship);
